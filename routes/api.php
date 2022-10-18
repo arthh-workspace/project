9a7803d7b990a8c22f6c58a17e9f61b5733777b4
RizalEka321 <hahaharizal6@gmail.com>
2022-10-18 18:52:00 +0700

    kerja

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ApiController;
+use App\Http\Controllers\LoginApiController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('login', [ApiController::class, 'login']);
+Route::post('/login', [LoginApiController::class, 'index']);
+Route::get('/logout', [LoginApiController::class, 'logout']);
+
