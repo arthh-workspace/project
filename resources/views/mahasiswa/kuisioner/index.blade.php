ee8a3e61cef8c97ad5ac9191e2aa7419664cd76a
endikj <endikjayy@gmail.com>
2023-01-12 10:20:56 +0700

    commit

--- resources/views/mahasiswa/kuisioner/index.blade.php
+++ resources/views/mahasiswa/kuisioner/index.blade.php
@@ -63,22 +63,70 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="id_rps_mingguan">Pertemuan</label>
-                                <select name="id_rps_mingguan" value="{{ old('pertemuan_ke') }}"
-                                    class="form-control @error('id_rps_mingguan') is-invalid @enderror">
-                                    <option value="">-- Pertemuan ke --</option>
-                                    @foreach ($pertemuan as $item)
-                                        <option value="{{ $item->id }}">Pertemuan ke {{ $item->pertemuan_ke }}
-                                        </option>
-                                    @endforeach
-                                </select>
-                                <div class="text-danger">
-                                    @error('id_rps_mingguan')
-                                        Pertemuan tidak boleh kosong.
-                                    @enderror
-                                </div>
+                                <label for="id_pertemuan">Pertemuan</label> <br>
+                                 <!--<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>-->
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script language="javascript" type="text/javascript">  
+    $(document).ready(function() {
+        //create arrays to store option and values
+        var continents = [
+            {display: "Mahasiswa mampu memahami materi Software Testing", value: "pertemuan1" },    
+            {display: "Mahasiswa mampu memahammi materi Proyek Aplikasi Lanjut", value: "pertemuan2" },
+            {display: "Antarctica", value: "antarctica" },
+            {display: "Australia", value: "australia" },
+            {display: "Europe", value: "europe" },
+            {display: "North America", value: "north-america" },
+            {display: "South America", value: "south-america" }
+        ];
+           
+        var oceans = [
+            {display: "Pacific", value: "pacific" },
+            {display: "Atlantic", value: "atlantic" },
+            {display: "Indian", value: "indian" },
+            {display: "Southern", value: "southern" },
+            {display: "Arctic", value: "arctic" }
+        ];
+
+        //If parent option is changed
+        $("#parent_selection").change(function() {
+            var parent = $(this).val(); //get option value from parent       
+            switch(parent){ //using switch compare selected option and populate child
+                case 'continents':
+                    $('#child_selection').attr('disabled', false);
+                    list(continents);
+                    break;
+                case 'oceans':
+                    $('#child_selection').attr('disabled', false);
+                    list(oceans);
+                    break;
+                default: //default child option is blank
+                    $("#child_selection").html('');
+                    $('#child_selection').attr("disabled", true);
+                    break;
+            }
+        });
+
+        //function to populate child select box
+        function list(array_list) {
+            $("#child_selection").html(""); //reset child options
+            $(array_list).each(function (i) { //populate child options
+                $("#child_selection").append("<option value='" + array_list[i].value + "'>" + array_list[i].display + "</option>");
+            });
+        }
+    });
+    </script>
+    <div class="wrapper">
+        {{-- Pertemuan : --}}
+        <select name="parent_selection" id="parent_selection">
+            <option value="">-- Pilih --</option>
+            <option value="continents">Pertemuan 1</option>
+            <option value="oceans">Pertemuan 2</option>
+        </select>
+        <select name="child_selection" id="child_selection" disabled="disabled"/>
+    </div>
                             </div>
-                            <div class="form-group">
+                        </div>
+                            {{-- <div class="form-group">
                                 <label for="capaian_pembelajaran_pertemuan">Capaian Materi</label>
                                 <select name="capaian_pembelajaran_pertemuan"
                                     value="{{ old('capaian_pembelajaran_pertemuan') }}"
@@ -94,9 +142,9 @@
                                         Capaian Materi tidak boleh kosong.
                                     @enderror
                                 </div>
-                            </div>
+                            </div> --}}
                             <div class="form-group">
-                                <label for="jenis_kelamin">Jenis Kelamin :</label> <br>
+                                <label for="kesesuaian">Kesesuaian :</label> <br>
                                 <div class="form-check form-check-inline">
                                     <label for="kesesuaian">
                                         <input type="radio" name="kesesuaian" value="S" id="kesesuaian">Sesuai
